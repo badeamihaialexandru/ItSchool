1e2dac9c76cda4ec055563392c5c0baee789c8c1
leionaaad <leionaaad@yahoo.com>
2023-11-09 21:01:57 +0200

    fisiere noi, inclusiv trisare

--- leo.py
+++ leo.py
@@ -1,7 +1,10 @@
 def addExtraordinaire(a, b) -> float:
+    #Azte e vechi
     return float(a + b)
 
 def subtractExtraordinaire(a, b) -> float:
     return float(a - b)
 
 #comentezi?
+def powerOf3(base):
+    return base*base*base
